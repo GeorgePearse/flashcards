73f2b71ac07518a848d53434514de29be464fec2
unknown <georgepearse@kubrickgroup.com>
2020-04-19 17:37:54 +0100

    database checking works and is quick@

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, request, jsonify, render_template, url_for, redirect
 import json
 import requests
 import pymongo
+import json
 
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 coll = client["local"]["test4"]
@@ -27,11 +28,32 @@ def insert():
     if request.method == 'GET':
         return render_template('form.html',user="George")
     if request.method == 'POST':
+        if request.name == 'insert':
+            text = request.form['text']
+            key,value = text.split('=')
+            coll.insert_one({key:value})
+            return render_template('insert_response.html',key=key,value=value)
+        
+@app.route('/check', methods=['GET','POST'])
+def check():
+    if request.method == 'POST':
         text = request.form['text']
-        key,value = text.split('=')
-        coll.insert_one({key:value})
-        return render_template('response.html',key=key,value=value)
+        collection = client["local"]["test4"].find_one({})
 
+        dict_response = {} # this is a horrible temporary workaround!!!
+        cursor = coll.find()
+        for record in cursor:
+            item = list(record.values())[1]
+            value = list(record.keys())[1]
+            if item not in list(record.keys()):
+                dict_response[item] = value
+            else:
+                pass
+
+        result = dict_response.get(text, 'item not in database')
+        if result != 'item not in database':
+            result += '     Selected card is in the deck'
+        return render_template('check_response.html',check=str(text),result=str(result))
 
 app.run(debug=True)
 
